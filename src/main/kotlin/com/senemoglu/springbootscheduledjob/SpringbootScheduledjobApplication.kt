746d34cb87f86c86ac7fdef625c2ecd4f7de2f2a
senemogluc <cagin.senemoglu@gmail.com>
2024-04-18 09:27:21 +0300

    first commit

---
+++ src/main/kotlin/com/senemoglu/springbootscheduledjob/SpringbootScheduledjobApplication.kt
@@ -0,0 +1,11 @@
+package com.senemoglu.springbootscheduledjob
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootScheduledjobApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootScheduledjobApplication>(*args)
+}
---
+++ src/test/kotlin/com/senemoglu/springbootscheduledjob/SpringbootScheduledjobApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.senemoglu.springbootscheduledjob
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootScheduledjobApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
