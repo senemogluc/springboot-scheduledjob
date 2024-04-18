a70b167cdb54534defbb2bd9657f6c0df1be9b07
senemogluc <cagin.senemoglu@gmail.com>
2024-04-18 10:23:01 +0300

    added scheduling and feign client annotations

--- src/main/kotlin/com/senemoglu/springbootscheduledjob/SpringbootScheduledjobApplication.kt
+++ src/main/kotlin/com/senemoglu/springbootscheduledjob/SpringbootScheduledjobApplication.kt
@@ -1,9 +1,15 @@
 package com.senemoglu.springbootscheduledjob
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableScheduling
+@EnableFeignClients
 class SpringbootScheduledjobApplication
 
 fun main(args: Array<String>) {
